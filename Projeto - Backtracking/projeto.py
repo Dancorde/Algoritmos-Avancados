c85a75a519e99b893f9d16b5e5255de404bdb435
Danilo Henrique Cordeiro <dancorde@gmail.com>
2018-09-10 14:51:38 -0300

    Arrumado o código - 10/10

--- Projeto - Backtracking/projeto.py
+++ Projeto - Backtracking/projeto.py
@@ -1,54 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import copy
-######################################
-#                                    #
-#    Função para ler o Arquivo       #
-#                                    #
-######################################
-def readFile(fileName):
-
-	file = open(fileName, 'r')
-
-	board = []
-
-	R = 0
-	C = 0
-	i = 0
-
-	for line in file:
-		if(i == 0):
-			R = int(line.split(" ")[0])
-			C = int(line.split(" ")[1])
-			i += 1
-		else:
-			board.append( [int(x) for x in line.split()])	
-
-	file.close()
-
-	return (R, C), board
-
-######################################
-#                                    #
-#  Função para imprimir o tabuleiro  #
-#                                    #
-######################################
-def printBoard(board):
-	for i in range(len(board)):
-		print(*board[i])
-
-
-######################################
-#                                    #
-#  Função para imprimir a solução    #
-#                                    #
-######################################
-def printSol(board):
-	for i in range(len(board)):
-		for j in range(len(board[i])):
-			if (board[i][j] != 0):
-				print(i, j, board[i][j])
-
+from operator import itemgetter, attrgetter
 
 ########################################
 #                                      #
@@ -81,7 +34,7 @@ def findSolutions(board, R, C):
 							board_copy[i][j] = 0
 							board_copy[i-1][j] = type
 
-							
+
 							findSolutions(board_copy, R, C)
 					#right
 					if j < C-1:
@@ -90,7 +43,7 @@ def findSolutions(board, R, C):
 							board_copy[i][j] = 0
 							board_copy[i][j+1] = type
 
-						
+
 							findSolutions(board_copy, R, C)
 						#down
 					if i < R-1:
@@ -99,7 +52,7 @@ def findSolutions(board, R, C):
 							board_copy[i][j] = 0
 							board_copy[i+1][j] = type
 
-							
+
 							findSolutions(board_copy, R, C)
 					#left
 					if j > 0:
@@ -108,7 +61,7 @@ def findSolutions(board, R, C):
 							board_copy[i][j] = 0
 							board_copy[i][j-1] = type
 
-						
+
 							findSolutions(board_copy, R, C)
 	else:
 		#save just the line, column and piece
@@ -118,16 +71,17 @@ def findSolutions(board, R, C):
 					final_states.append(str(line+1) + " " + str(column+1) + " " +str(board[line][column]))
 					break
 		#final_states.append("".join(([str(item) for line in board for item in line])))
+
 def order_final(final_states):
 	final_states = [item.split(r" ")  for item in final_states]
 	#print(final_states)
-	final = sorted(final_states, key= len, reverse=False)
+	final = sorted(final_states)
 	for item in final:
 		print(*item)
-	
+
 
 def move(from_piece, to_piece):
-	
+
 
 	if from_piece == 1:
 		if to_piece == 2 :
@@ -146,16 +100,17 @@ def move(from_piece, to_piece):
 #                                      #
 ########################################
 if __name__ == "__main__":
-	info = readFile(sys.argv[1])
-	[R, C] = info[0]
-	board = info[1]
-	
+	board = []
+	[R, C] = [int(x) for x in input().split()]
+
+	for i in range(R):
+		board.append([int(x) for x in input().split()])
 
 	findSolutions(board, R, C)
 	print(len(final_states))
 	print(len(set(final_states)))
 	order_final(list(set(final_states)))
-	#printBoard(board)
+	# printBoard(board)
 	#printSol(board)
 
 #	print(endGame(board, R, C))
