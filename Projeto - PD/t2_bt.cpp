5e40f9d11e991c9df704b9ddedf142063d9d5837
Matheus Sanchez <matheus2.sanchez@usp.br>
2018-11-18 14:52:41 -0200

    traduzindo o código do annitaa pra c++ pra ver se da uma ajuda

--- Projeto - PD/t2_bt.cpp
+++ Projeto - PD/t2_bt.cpp
@@ -3,11 +3,40 @@
 using namespace std;
 
 
+void p_debug(vector< vector<int> > v){
+	for (int i = 0; i < v.size(); ++i){
+		for (int j = 0; j < v[i].size(); ++j){
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+void acha_solucao(vector< vector<int> > v, int l, int c){
+
+
+
+}
+
+
 int main (){
 
 	ios :: sync_with_stdio(false);
 
-	int l,c;
+	int l,c,aux;
 	cin >> l >> c;
+	
+	vector< vector<int> > v(l);
+
+	for (int i = 0; i < l; ++i){
+		for (int j = 0; j < c; ++j){
+			cin >> aux;
+			v[i].push_back(aux);
+		}
+	}
+	p_debug(v);	
+	
+
 
 }
